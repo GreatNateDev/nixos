6dd2c9cf09457926ee8da494f20231c0f4619f40
GreatNateDev <Nate@pelmel.net>
2025-01-21 21:11:07 -0600

    this will be fun

---
+++ configuration.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  #Networking
+  networking.hostName = "Nate-Landises-Computer";
+  networking.networkmanager.enable = true;
+  # Locales
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  # Desktop ENV
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  # Printing
+  services.printing.enable = true;
+  # Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  # Users and user packages
+  users.users.nate = {
+    isNormalUser = true;
+    description = "Nathaniel Landis";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  # Firefox
+  programs.firefox.enable = true;
+  #Main Packages
+  environment.systemPackages = with pkgs; [
+  neovim
+  git
+  github-desktop
+  xdg-utils
+  xdg-desktop-portal
+  xdg-desktop-portal-gtk
+  xdg-desktop-portal-kde
+  ];
+  # Services
+  services.openssh.enable = true;
+  networking.firewall.enable = false;
+   xdg = {
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-kde
+      ];
+    };
+  };
+  #Version
+  system.stateVersion = "24.11";
+}
