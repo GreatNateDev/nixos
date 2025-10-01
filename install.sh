8475e4ab85b79bbdaa3b954ae21d87746308d472
Nate <Nate@pelmel.net>
2025-10-01 14:44:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.config/nixos
 mv ~/nixos/.git ~/.config/nixos/
-mv ~/nixos/nixos ~/.config/nixos/
+mv ~/nixos/* ~/.config/nixos/
 sudo mv /etc/nixos/hardware-configuration.nix ~/.config/nixos/hardware-configuration.nix
 sudo rm -rf /etc/nixos/
 sudo nixos-rebuild switch --flake $HOME/.config/nixos
