fecf3bd23715f3bc46968acc0965ddca85eb886e
GreatNateDev <Nate@pelmel.net>
2025-10-02 13:25:31 -0500

    add lxqt for a backup

--- display.nix
+++ display.nix
@@ -1,15 +1,23 @@
 { ... }:
 {
-  services.displayManager.gdm.enable = true;
+  services.displayManager = {
+    sddm.enable = true;
+    defaultSession = "niri";
+    # Disable GDM in case it lingers
+    gdm.enable = false;
+  };
+
+  # X server
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us"; # keyboard layout
+    desktopManager.lxqt.enable = true;
+    # Set video drivers explicitly to avoid blank screen
+    # Change "intel" to "nvidia" or "amdgpu" depending on your GPU
+    #videoDrivers = [ "amdgpu" ];
+  };
   programs.niri = {
     enable = true;
-  };
-  environment.etc = {
-    "AccountsService/users/nate".text = ''
-      [User]
-      Icon=/etc/AccountsService/icons/nate
-    '';
-    "AccountsService/icons/nate".source = /home/nate/.face;
   };
 
 }
