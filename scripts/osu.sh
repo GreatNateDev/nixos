6384fa82300970f109cee94a95e8c42c7f6dbb4e
GreatNateDev <Nate@pelmel.net>
2025-10-04 20:26:57 -0500

    patch osu script

--- scripts/osu.sh
+++ scripts/osu.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/GreatNateDev/osu.git
 cd osu
 sudo rm -rf .git
-mv * /home/nate/.local/share/osu/
+sudo mv * /home/nate/.local/share/osu/
 cd /home/nate/.local/share/osu/
 sudo mkdir -p Skins
-cp -r s/* Skins/
+sudo cp -r s/* Skins/
 cd ~
