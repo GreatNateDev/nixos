c1ddbdae4601fcb890b3292191feeb9bcf1bc163
GreatNateDev <Nate@pelmel.net>
2025-10-23 18:07:19 -0500

    cool logo soon

--- nix/System/Boot.nix
+++ nix/System/Boot.nix
@@ -1,5 +1,32 @@
-{ ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  nixosSpinnerTheme = pkgs.stdenv.mkDerivation {
+    name = "plymouth-nixos-spinner";
+    src = ../../data/plymouth; # Directory with your theme files
+    buildInputs = [ pkgs.plymouth ];
+    installPhase = ''
+      mkdir -p $out/share/plymouth/themes/nixos-spinner
+      cp -r * $out/share/plymouth/themes/nixos-spinner/
+    '';
+  };
+in
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth = {
+    enable = true;
+    theme = "nixos-spinner";
+    themePackages = [ nixosSpinnerTheme ];
+  };
+
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+  ];
 }
