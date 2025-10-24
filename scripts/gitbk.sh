aab9b623987050d7adc4345709dd12ea22aaa918
GreatNateDev <Nate@pelmel.net>
2025-10-24 18:37:06 -0500

    improve gitbk

--- scripts/gitbk.sh
+++ scripts/gitbk.sh
@@ -1,1 +1,5 @@
- gh repo list GreatNateDev --limit 1000 --json nameWithOwner -q '.[].nameWithOwner' | xargs -L1 gh repo clone
+rm -rf *
+EXCLUDE="" # In format Owner/Repo
+gh repo list GreatNateDev --limit 1000 --json nameWithOwner,isFork -q '.[] | select(.isFork == false) | .nameWithOwner' | \
+grep -Ev "$EXCLUDE" | \
+xargs -L1 gh repo clone
