88d0b0e19aa5a48d9d561e0345c4c1f390c4fb16
GreatNateDev <Nate@pelmel.net>
2025-10-23 19:05:59 -0500

    final fix

--- nix/System/Boot.nix
+++ nix/System/Boot.nix
@@ -4,11 +4,10 @@
   lib,
   ...
 }:
-
 let
   nixosSpinnerTheme = pkgs.stdenv.mkDerivation {
     name = "plymouth-nixos-spinner";
-    src = ../../data/plymouth; # Directory with your theme files
+    src = ../../data/plymouth;
     buildInputs = [ pkgs.plymouth ];
     installPhase = ''
       mkdir -p $out/share/plymouth/themes/nixos-spinner
@@ -19,14 +18,22 @@ in
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
   boot.plymouth = {
     enable = true;
     theme = "nixos-spinner";
     themePackages = [ nixosSpinnerTheme ];
   };
 
+  # Enable systemd in initrd for early Plymouth
+  boot.initrd.systemd.enable = true;
+
   boot.kernelParams = [
     "quiet"
     "splash"
+    "vt.global_cursor_default=0"
   ];
+
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
 }
