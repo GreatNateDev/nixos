3d4f8cd58bae4fdd5da6c2b10c6ef61d41720d75
GreatNateDev <nate@pelmel.net>
2025-10-23 19:42:08 -0500

    fix installer

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 clear
 
 cat <<"EOF"
-███    ██  █████  ████████ ███████ ███████     ███    ██ ██ ██   ██  ██████  ███████     ██ ███    ██ ███████ ████████  █████  ██      ██      ███████ ██████  
-████   ██ ██   ██    ██    ██      ██          ████   ██ ██  ██ ██  ██    ██ ██          ██ ████   ██ ██         ██    ██   ██ ██      ██      ██      ██   ██ 
-██ ██  ██ ███████    ██    █████   ███████     ██ ██  ██ ██   ███   ██    ██ ███████     ██ ██ ██  ██ ███████    ██    ███████ ██      ██      █████   ██████  
-██  ██ ██ ██   ██    ██    ██           ██     ██  ██ ██ ██  ██ ██  ██    ██      ██     ██ ██  ██ ██      ██    ██    ██   ██ ██      ██      ██      ██   ██ 
-██   ████ ██   ██    ██    ███████ ███████     ██   ████ ██ ██   ██  ██████  ███████     ██ ██   ████ ███████    ██    ██   ██ ███████ ███████ ███████ ██   ██ 
-                                                                                                                                                               
-                                                                                                                                                                                                                                                                                                                                  
-                                                                                                                                                                                                                                                                                                                                                                     
+███    ██  █████  ████████ ███████ ███████     ███    ██ ██ ██   ██  ██████  ███████     ██ ███    ██ ███████ ████████  █████  ██      ██      ███████ ██████
+████   ██ ██   ██    ██    ██      ██          ████   ██ ██  ██ ██  ██    ██ ██          ██ ████   ██ ██         ██    ██   ██ ██      ██      ██      ██   ██
+██ ██  ██ ███████    ██    █████   ███████     ██ ██  ██ ██   ███   ██    ██ ███████     ██ ██ ██  ██ ███████    ██    ███████ ██      ██      █████   ██████
+██  ██ ██ ██   ██    ██    ██           ██     ██  ██ ██ ██  ██ ██  ██    ██      ██     ██ ██  ██ ██      ██    ██    ██   ██ ██      ██      ██      ██   ██
+██   ████ ██   ██    ██    ███████ ███████     ██   ████ ██ ██   ██  ██████  ███████     ██ ██   ████ ███████    ██    ██   ██ ███████ ███████ ███████ ██   ██
+
+
+
 EOF
 # Ensure hostname is 'nixos'
 if [ "$(hostname)" != "nixos" ]; then
@@ -23,26 +23,37 @@ if [ "$USER" != "nate" ]; then
   echo "    Current user: $USER"
   exit 1
 fi
+nixos-generate-config
 echo "Starting config installer..."
-echo "What is your gpu type... Accepted Values amdgpu, nvidia, intel, vmware, virtualbox, qxl, virtio"
-read GPU
 echo "Making Directory..."
 mkdir -p ~/.config/nixos
 echo "Moving files..."
 mv ~/nixos/.git ~/.config/nixos/
 mv ~/nixos/.gitignore ~/.config/nixos/
 mv ~/nixos/* ~/.config/nixos/
+rmdir ~/nixos
+rmdir ~/Desktop
+rmdir ~/Documents
+rmdir ~/Downloads
+rmdir ~/Music
+rmdir ~/Pictures
+rmdir ~/Public
+rmdir ~/Templates
+rmdir ~/Videos
 echo "Fixing Issues"
 rm ~/.config/user-dirs.dirs
 echo "Getting hardware config..."
 sudo mv /etc/nixos/hardware-configuration.nix ~/.config/nixos/nix/hardware.nix
-echo "Adding your gpu..."
-sed -i '/boot\.initrd\.kernelModules/d' "/home/$USER/.config/nixos/nix/hardware.nix"
-gpucode="boot.initrd.kernelModules = [\"$GPU\"];"
-sed -i "/^}$/i $gpucode" "/home/$USER/.config/nixos/nix/hardware.nix"
 echo "Removing old config..."
 sudo rm -rf /etc/nixos/
 echo "Building..."
 echo 'Rebuilding NixOS...' && sudo nixos-rebuild switch --flake $HOME/.config/nixos/nix --impure --quiet && echo Done!
-echo "Script complete rebooting..."
-sudo reboot
+echo "Script complete Reboot? (Y/n)"
+read rebootcon
+if $rebootcon == n
+    exit
+fi
+if $rebootcon == y
+    reboot
+fi
+reboot
