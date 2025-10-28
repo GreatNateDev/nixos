5412b0771ad03a39bb5c772d530c52bbfbd80ab9
GreatNateDev <Nate@pelmel.net>
2025-10-28 05:14:13 -0500

    update the bad script

--- scripts/osu.sh
+++ scripts/osu.sh
@@ -4,11 +4,7 @@ repo="osu.git"
 path="$HOME/.local/share/osu"
 git clone https://github.com/$gituser/$repo
 cd osu
-sudo rm -rf .git
+rm -rf $path
 sudo mv * $path
-cd $path
-sudo mkdir -p Skins
-sudo cp -r s/* Skins/
-cd ~/.config/nixos/scripts
+cd ..
 rm -rf osu
-nautilus $path/Skins/
