59d506b5a74e4bf9e40ff3e5d993bd5e3ae6f90d
GreatNateDev <Nate@pelmel.net>
2025-10-30 02:30:55 -0500

    remove i3

--- nix/System/Services.nix
+++ nix/System/Services.nix
@@ -1,8 +1,8 @@
 { config, ... }:
 let
-actualUser = builtins.getEnv "SUDO_USER";
-user = if actualUser != "" then actualUser else builtins.getEnv "USER";
-env = import /home/${user}/.config/nixos/nix/env.nix;
+  actualUser = builtins.getEnv "SUDO_USER";
+  user = if actualUser != "" then actualUser else builtins.getEnv "USER";
+  env = import /home/${user}/.config/nixos/nix/env.nix;
 in
 {
   hardware.bluetooth.enable = true;
@@ -15,13 +15,6 @@ in
         theme = "catppuccin-moha-red";
       };
       defaultSession = "hyprland";
-      gdm.enable = false;
-    };
-    xserver = {
-      enable = true;
-      windowManager.i3.enable = true;
-      windowManager.i3.configFile = "/home/${env.username}/.config/nixos/data/i3/config";
-      videoDrivers = config.boot.initrd.kernelModules;
     };
     pipewire = {
       enable = true;
