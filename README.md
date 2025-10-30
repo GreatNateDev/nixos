a4c3aaa6168c0a630b6e374dfa69facc7e1b1b67
GreatNateDev <Nate@pelmel.net>
2025-10-30 16:47:36 -0500

    fix errors

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Installing
 Install NixOS from the ISO then run this command:
 ```
-nix-env -iA nixos.git && git clone https://github.com/GreatNateDev/nixos.git && bash ~/nixos/install.zsh
+nix-env -iA nixos.git && git clone https://github.com/GreatNateDev/nixos.git && bash ~/nixos/install
 ```
--- scripts/post-install.zsh
+++ scripts/post-install.zsh
@@ -1,11 +1,11 @@
 #manually run this script to finish setup by downloading game progress and signing into personal accounts
-
-read -p "Setup GitHub? (Y/n) " githubconfirm
+echo -n "Setup GitHub? (Y/n) "
+read githubconfirm
 if [ "$githubconfirm" != "n" ]; then
   ./github.zsh
 fi
-
-read -p "Setup OSU? (y/N) " osuconfirm
+echo -n "Setup OSU? (y/N) "
+read osuconfirm
 if [ "$osuconfirm" = "y" ]; then
   ./osu.zsh
 fi
