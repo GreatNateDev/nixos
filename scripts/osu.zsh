4c611dd2c69f9ac33e1cdc1d96e18855a8bfa832
GreatNateDev <Nate@pelmel.net>
2025-10-30 17:05:04 -0500

    fix

--- scripts/osu.zsh
+++ scripts/osu.zsh
@@ -1,5 +1,5 @@
 # basicly setup a private repo with all your entire osu path commited
-source "$HOME/.config/nixos/nix/getenv.zsh"
+source "$HOME/.config/nixos/scripts/getenv.zsh"
 repo="osu.git"
 path="$HOME/.local/share/osu"
 git clone https://github.com/$gituser/$repo
