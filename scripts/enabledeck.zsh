d7debafc4cd975b4d5c7674b14626b76f9629f94
GreatNateDev <Nate@pelmel.net>
2025-11-03 12:37:14 -0600

    Revert Steam Deck Build

--- scripts/enabledeck.zsh
+++
@@ -1,6 +0,0 @@
-sed -i '/\.\/System\/Font\.nix/a\    (import /home/${env.username}/.config/nixos/nix/deck.nix)' ~/.config/nixos/nix/System.nix
-sed -i '/\.\/Home\/Systemd\.nix/a\    (import /home/${env.username}/.config/nixos/nix/deck-home.nix)' ~/.config/nixos/nix/Home.nix
-echo "System.nix" >>~/.config/nixos/.gitignore
-echo "Home.nix" >>~/.config/nixos/.gitignore
-cp files/deck.nix ~/.config/nixos/nix/deck.nix
-cp files/deck-home.nix ~/.config/nixos/nix/deck-home.nix
