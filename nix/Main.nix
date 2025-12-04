daa6d270555ba7585fc294f76eb73f2eec7a2979
GreatNateDev <Nate@pelmel.net>
2025-12-04 03:34:10 -0600

    Fix Wallpaper folder escaping

--- nix/Main.nix
+++ nix/Main.nix
@@ -86,15 +86,16 @@ in
       home.stateVersion = "25.11";
     };
 
-  # Global Nix settings
-  nix.extraOptions = ''
-    warn-dirty = false
-  '';
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
+    # Global Nix settings
+    nix.extraOptions = ''
+      warn-dirty = false
+    '';
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
 }
