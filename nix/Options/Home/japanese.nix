f62e7e2e82b524bc3e70ac3a0d465ae943d4d39f
GreatNateDev <Nate@pelmel.net>
2026-02-18 17:48:14 -0600

    Hide useless desktop entires

--- nix/Options/Home/japanese.nix
+++ nix/Options/Home/japanese.nix
@@ -24,5 +24,69 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    xdg.desktopEntries = {
+
+      "fcitx5-configtool" = {
+        name = "Fcitx5 Config Tool";
+        exec = "fcitx5-configtool";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "fcitx5-wayland-launcher" = {
+        name = "Fcitx5 Wayland Launcher";
+        exec = "fcitx5-wayland-launcher";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "kcm_fcitx5" = {
+        name = "Fcitx5 KDE Control Module";
+        exec = "kcmshell5 kcm_fcitx5";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "org.fcitx.Fcitx5" = {
+        name = "Fcitx5";
+        exec = "fcitx5";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "org.fcitx.fcitx5-migrator" = {
+        name = "Fcitx5 Migrator";
+        exec = "fcitx5-migrator";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "org.fcitx.fcitx5-qt5-gui-wrapper" = {
+        name = "Fcitx5 Qt5 GUI Wrapper";
+        exec = "fcitx5-qt5-gui-wrapper";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "org.fcitx.fcitx5-config-qt" = {
+        name = "Fcitx5 Qt Config";
+        exec = "fcitx5-config-qt";
+        type = "Application";
+        noDisplay = true;
+      };
+
+      "org.fcitx.fcitx5-qt6-gui-wrapper" = {
+        name = "Fcitx5 Qt6 GUI Wrapper";
+        exec = "fcitx5-qt6-gui-wrapper";
+        type = "Application";
+        noDisplay = true;
+      };
+      "kbd-layout-viewer5" = {
+        name = "Keyboard Layout Viewer";
+        exec = "kbd-layout-viewer5";
+        type = "Application";
+        noDisplay = true;
+      };
+    };
   };
 }
