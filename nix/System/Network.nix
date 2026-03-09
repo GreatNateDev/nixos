93afaaf1435bbb9c5b9771d9366f379dab476694
GreatNateDev <Nate@pelmel.net>
2026-03-09 17:20:35 -0500

    finally fix network stack yay

--- nix/System/Network.nix
+++ nix/System/Network.nix
@@ -3,97 +3,100 @@
 let
   actualUser = builtins.getEnv "SUDO_USER";
   user = if actualUser != "" then actualUser else builtins.getEnv "USER";
+
   env = import /home/${user}/.config/nixos/nix/env.nix;
+
   hasStaticIp = env.address != "";
+
+  ipv4Config =
+    if hasStaticIp then
+      {
+        method = "manual";
+        address1 = "${env.address}/24";
+        gateway = env.gateway;
+
+        dns = env.dns;
+        ignore-auto-dns = true;
+        ignore-auto-domain = true;
+      }
+    else
+      {
+        method = "auto";
+
+        dns = env.dns;
+        ignore-auto-dns = true;
+        ignore-auto-domain = true;
+      };
+
+  mkProfile =
+    base:
+    base
+    // {
+      ipv4 = ipv4Config;
+      ipv6.method = "ignore";
+    };
+
 in
 {
   networking = {
     networkmanager.enable = true;
     firewall.enable = true;
     useDHCP = false;
-    nameservers = [
-      "192.168.1.135"
+
+    nameservers = [ env.dns ];
+
+    hosts."127.0.0.1" = [
+      "jelly"
+      "gpt"
+      "fmhy"
+      "git"
+      "osu"
+      "q"
+      "yt"
+      "disc"
+      "mail"
+      "gm"
+      "server"
+      "rss"
+      "torrent"
+      "password"
+      "tr"
+      "code"
     ];
-    # TO FIX allow for users to NOT have a pihole server
-    #++ lib.optional config.windowmanager.enable "127.0.0.1";
-    hosts = {
-      "127.0.0.1" = [
-        "jelly"
-        "gpt"
-        "fmhy"
-        "git"
-        "osu"
-        "q"
-        "yt"
-        "disc"
-        "mail"
-        "gm"
-        "server"
-        "rss"
-        "torrent"
-        "password"
-        "tr"
-        "code"
-      ];
-    };
+
     networkmanager.ensureProfiles.profiles = lib.mkIf config.managenet.enable {
-      "main-ethernet" = {
+
+      main-ethernet = mkProfile {
         connection = {
           id = "main-ethernet";
           type = "ethernet";
-          interface-name = "${env.ethinterface}";
-          autoconnect = "true";
-          autoconnect-priority = "10";
-        };
-        ipv4 =
-          if hasStaticIp then
-            {
-              method = "manual";
-              address1 = "${env.address}/24";
-              gateway = "${env.gateway}";
-              dns = "192.168.1.135";
-            }
-          else
-            {
-              method = "auto";
-              dns = "192.168.1.135";
-            };
-        ipv6 = {
-          method = "ignore";
+          interface-name = env.ethinterface;
+
+          autoconnect = true;
+          autoconnect-priority = 10;
         };
       };
-      "main-wifi" = {
+
+      main-wifi = mkProfile {
         connection = {
           id = "main-wifi";
           type = "wifi";
-          autoconnect = "true";
-          autoconnect-priority = "20";
+
+          autoconnect = true;
+          autoconnect-priority = 20;
         };
+
         wifi = {
           ssid = env.ssid;
           mode = "infrastructure";
         };
+
         wifi-security = {
           key-mgmt = "wpa-psk";
           psk = env.netkey;
         };
-        ipv4 =
-          if hasStaticIp then
-            {
-              method = "manual";
-              address1 = "${env.address}/24";
-              gateway = "${env.gateway}";
-              dns = "192.168.1.135";
-            }
-          else
-            {
-              method = "auto";
-              dns = "192.168.1.135";
-            };
-        ipv6 = {
-          method = "ignore";
-        };
       };
+
     };
   };
 }
